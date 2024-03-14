33aac7fedc961ffe0bd18f300aa3d30367ee430b
proffapt <proffapt@pm.me>
2024-03-14 23:56:20 +0530

    feat: .env for the project

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,17 @@
+import os
 from api import api
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{POSTGRES_PASSWORD}@scmb-db:5432/{POSTGRES_DB}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 app.register_blueprint(api)
