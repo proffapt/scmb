413e0053673eb2fc58fa9088819d0a05883d825c
proffapt <proffapt@pm.me>
2024-03-15 01:11:40 +0530

    feat: error and product models

---
+++ webapp/models/error.py
@@ -0,0 +1,9 @@
+from typing import TypedDict
+
+
+class DB_Error(TypedDict):
+    db_error: str
+
+
+class API_Error(TypedDict):
+    api_error: str
--- webapp/models/product.py
+++ webapp/models/product.py
@@ -1,6 +1,6 @@
-from typing import TypedDict
+from database import db
 
 
-class Product(TypedDict):
-    code: str
-    name: str
+class Product(db.Model):
+    code = db.Column(db.String(50), primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
