b97d09e9dbd1ef3092e8b7f44f29631e300330b2
proffapt <proffapt@pm.me>
2024-04-04 12:55:23 +0530

    fix: CORS

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,5 @@
 import os
+from flask_cors import CORS
 from api import api
 from flask import Flask
 from database import db
@@ -11,5 +12,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{POSTGRES_PASSWORD}@scmb-db:5432/{POSTGRES_DB}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+CORS(app)
 db.init_app(app)
 app.register_blueprint(api)
